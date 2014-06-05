fef5735721a43955989489a6e41f816cb1bbca8d
Ivan Branets <ivan.branets@eltrovo.com>
2014-06-05 13:47:54 +0300

    first commit

---
+++ MsFakesSample/LogWritter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MsFakesSample
+{
+    public class Log
+    {
+        public Log(string message)
+        {
+            Message = message;
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public DateTime CreatedDate { get; private set; }
+        public string Message { get; private set; }
+    }
+
+    public interface ILogWritter
+    {
+        Log AddLog();
+    }
+
+    public class LogWritter : ILogWritter
+    {
+        public LogWritter(IDataProvider dataProvider, IFileWriter fileWriter)
+        {
+            _dataProvider = dataProvider;
+            _fileWriter = fileWriter;
+        }
+
+        private readonly IDataProvider _dataProvider;
+        private readonly IFileWriter _fileWriter;
+
+        public Log AddLog()
+        {
+            var message = _dataProvider.GetData();
+            var log = new Log(message);
+
+            _fileWriter.WriteFile(log);
+
+            return log;
+        }
+    }
+
+    public interface IDataProvider
+    {
+        string GetData();
+    }
+
+    public interface IFileWriter
+    {
+        bool WriteFile(object obj);
+    }
+}
+
+
---
+++ MsFakesSampleTests/LogWritterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsFakesSample;
+
+namespace MsFakesSampleTests
+{
+    [TestClass]
+    public class LogWritterTests
+    {
+        [TestMethod]
+        public void WriteTest()
+        {
+            const string logData = "test message";
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.UtcNowGet = () => new DateTime(2012, 6, 5);
+
+                var fileWriter = new MsFakesSample.Fakes.StubIFileWriter();
+                var dataProvider = new MsFakesSample.Fakes.StubIDataProvider
+                {
+                    GetData = () => logData
+                };
+
+                var logWritter = new LogWritter(dataProvider, fileWriter);
+
+                var expected = new Log(logData);
+                var actual = logWritter.AddLog();
+
+                Assert.AreEqual(expected.Message, actual.Message);
+                Assert.AreEqual(expected.CreatedDate, actual.CreatedDate);                
+            }
+        }
+    }
+}
+
+
